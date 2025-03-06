d3fc1e2b89650300cec3cf9c40e36423e35fe9f6
thiagoqualy2k25 <thiagoqualy@gmail.com>
2025-03-06 02:42:46 +0000

    terraform app

--- app/deploy/deploy.tf
+++ app/deploy/deploy.tf
@@ -2,18 +2,26 @@ data "aws_lb_target_group" "this" {
   name = "app-prod-tg"
 }
 
-resource "aws_clowdwatch_log_group" "this" {
-  name              = "/ecs/ci-cd-app"
-  retention_in_days = 7
+data "aws_security_groups" "selected" {
+  filter {
+    name   = "tag:Name"
+    values = ["app-prod-sg"]
+  }
 }
-
 
 resource "aws_ecs_service" "this" {
   name            = "app-service"
+  task_definition = "ci-cd-app"
   cluster         = var.cluster_name
-  task_definition = aws_ecs_task_definition.this.arn
-  desired_count   = 3
+  desired_count   = var.desired_count
   launch_type     = "FARGATE"
+  availability_zone_rebalancing = "ENABLED"
+  network_configuration {
+    subnets          = var.subnets
+    security_groups  = var.security_groups
+    assign_public_ip = true
+  }
+
 
   load_balancer {
     target_group_arn = aws_lb_target_group.foo.arn
@@ -23,81 +31,7 @@ resource "aws_ecs_service" "this" {
 
 }
 
-resource "aws_ecs_task_definition" "this" {
-  family                   = "app-ci-cd"
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  cpu                      = 1024
-  memory                   = 2048
-  container_definitions    = <<TASK_DEFINITION
-[
-  {
-      "containerDefinitions": [
-          {
-              "name": "ci-cd-app",
-              "image": "gersontpc/ci-cd-app:v1.0.0",
-              "cpu": 0,
-              "portMappings": [
-                  {
-                      "name": "port-8000",
-                      "containerPort": 8000,
-                      "hostPort": 8000,
-                      "protocol": "tcp",
-                      "appProtocol": "http"
-                  }
-              ],
-              "essential": true,
-              "environment": [],
-              "environmentFiles": [],
-              "mountPoints": [],
-              "volumesFrom": [],
-              "ulimits": [],
-              "logConfiguration": {
-                  "logDriver": "awslogs",
-                  "options": {
-                      "awslogs-group": "/ecs/ci-cd-app",
-                      "mode": "non-blocking",
-                      "awslogs-create-group": "true",
-                      "max-buffer-size": "25m",
-                      "awslogs-region": "us-east-1",
-                      "awslogs-stream-prefix": "ecs"
-                  },
-                  "secretOptions": []
-              },
-              "systemControls": [],
-              "healthCheck": {
-                  "command": ["CMD-SHELL", "curl -f http://0.0.0.0:8000 || exit 1"],
-                  "interval": 15,
-                  "timeout": 5,
-                  "retries": 3,
-                  "startPeriod": 20
-              }
-          }
-      ],
-      "family": "ci-cd-app",
-      "taskRoleArn": "arn:aws:iam::526926919628:role/LabRole",
-      "executionRoleArn": "arn:aws:iam::526926919628:role/LabRole",
-      "networkMode": "awsvpc",
-      "volumes": [],
-      "placementConstraints": [],
-      "requiresCompatibilities": [
-          "FARGATE"
-      ],
-      "cpu": "512",
-      "memory": "1024",
-      "runtimePlatform": {
-          "cpuArchitecture": "X86_64",
-          "operatingSystemFamily": "LINUX"
-      },
-      "tags": [
-          {
-              "key": "Name",
-              "value": "ci-cd-app"
-          }
-      ]
-  }
-]
-TASK_DEFINITION
-
+resource "aws_clowdwatch_log_group" "this" {
+  name              = "/ecs/ci-cd-app"
+  retention_in_days = 7
 }
-
--- app/deploy/variables.tf
+++ app/deploy/variables.tf
@@ -1,1 +1,21 @@
-variable "cluster_name" {}
+variable "cluster_name" {
+  description = "Nome do cluster ECS"
+  type        = string
+  default = "app-prod"
+}
+
+variable "desired_count" {
+  description = "desired tasks"
+  type   = number
+  default = 3
+}
+
+variable "subnets_id" {
+  description = "Subnets IDs"
+  type        = list(string)
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
